9dc8c207ec8d8b6e6814272d575203a126c735c2
Wanguy <w020283852@gmail.com>
2017-10-31 09:26:36 +0800

    Commit program

---
+++ Checklists/AppDelegate.swift
@@ -0,0 +1,75 @@
+//
+//  AppDelegate.swift
+//  Checklists
+//
+//  Created by Richard Wang on 10/20/17.
+//  Copyright © 2017 Richard Wang. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+    let dataModel = DataModel()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let navigationController = window!.rootViewController as! UINavigationController
+        let controller = navigationController.viewControllers[0] as! AllListViewController
+        controller.dataModel = dataModel
+        let center = UNUserNotificationCenter.current()
+//        center.requestAuthorization(options: [.alert, .sound], completionHandler: {
+//            granted, error in
+//            if granted {
+//                print("We have permission")
+//            } else {
+//                print("Permission deied")
+//            }
+//        })
+//        let content = UNMutableNotificationContent()
+//        content.title = "Hello"
+//        content.body = "I am a local notifcation"
+//        content.sound = UNNotificationSound.default()
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+//        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
+//        center.add(request)
+        center.delegate = self
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        saveData()
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        saveData()
+    }
+
+    func saveData() {
+        dataModel.saveChecklists()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received loacl notification \(notification)")
+    }
+
+    
+}
+
